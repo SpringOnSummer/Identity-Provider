30de208ccf42f3d102c710e766002ce6245cb59d
Lee Seong Jun <devel0624@gmail.com>
2023-05-15 10:03:04 +0900

    Initial commit

---
+++ src/test/java/org/project/personal/identity_provider/IdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.personal.identity_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
