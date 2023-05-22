cf0fbad5498138e72e938b235248a58000cc64f1
Lee Seong Jun <devel0624@gmail.com>
2023-05-22 14:44:10 +0900

    [Feature] MemberDetail Entity & Repository #2

---
+++ src/main/java/org/project/personal/identity_provider/repository/MemberDetailRepository.java
@@ -0,0 +1,8 @@
+package org.project.personal.identity_provider.repository;
+
+import org.project.personal.identity_provider.entity.MemberDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberDetailRepository extends JpaRepository<MemberDetail, Long> {
+
+}
