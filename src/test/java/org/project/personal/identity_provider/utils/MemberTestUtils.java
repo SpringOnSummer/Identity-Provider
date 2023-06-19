cfb9461f60c7e1149251f638fcbfe5b427b8057e
Lee Seong Jun <devel0624@gmail.com>
2023-06-19 12:31:27 +0900

    [Test] Member Service 테스트
    BDD Mockito 와 hamcrest 적용

--- src/test/java/org/project/personal/identity_provider/utils/MemberTestUtils.java
+++ src/test/java/org/project/personal/identity_provider/utils/MemberTestUtils.java
@@ -2,8 +2,11 @@ package org.project.personal.identity_provider.utils;
 
 import org.project.personal.identity_provider.dto.request.JoinRequest;
 import org.project.personal.identity_provider.dto.request.MemberEmail;
+import org.project.personal.identity_provider.dto.request.MemberPasswordModifyRequest;
 import org.project.personal.identity_provider.entity.Member;
 import org.project.personal.identity_provider.entity.MemberDetail;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
@@ -30,12 +33,20 @@ public class MemberTestUtils {
 
         ReflectionTestUtils.setField(joinRequest, "email", "test@email.com");
         ReflectionTestUtils.setField(joinRequest, "memberName","test_member");
-        ReflectionTestUtils.setField(joinRequest, "password","password");
-
+        ReflectionTestUtils.setField(joinRequest, "password", "password");
         ReflectionTestUtils.setField(joinRequest, "phoneNumber","010-1234-5678");
         ReflectionTestUtils.setField(joinRequest, "nickName","nickName");
 
         return joinRequest;
+    }
+
+    public static MemberPasswordModifyRequest getMemberPasswordModifyRequest(){
+        MemberPasswordModifyRequest request = new MemberPasswordModifyRequest();
+
+        ReflectionTestUtils.setField(request, "currentPassword", "currentPassword");
+        ReflectionTestUtils.setField(request, "resetPassword", "resetPassword");
+
+        return request;
     }
 
     public static Member getMember(){
@@ -43,12 +54,18 @@ public class MemberTestUtils {
 
         MemberEmail email = MembersEmailUtils.convertEmail(joinRequest.getEmail());
 
-        return Member.builder()
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        Member member = Member.builder()
                 .memberName(joinRequest.getMemberName())
                 .emailLocal(email.getLocal())
                 .emailDomain(email.getDomain())
-                .password(joinRequest.getPassword())
+                .password(passwordEncoder.encode(joinRequest.getPassword()))
                 .build();
+
+        ReflectionTestUtils.setField(member, "id", 100L);
+
+        return member;
     }
 
     public static MemberDetail getMemberDetail(){
